368012bdd39b12c5ca7f35770a1809c0fe13e1fd
MAHARJAN, NEELESH (LNG-CON) <maharjan@legal.regn.net>
2026-05-07 12:39:14 -0400

    fix: linting issues like whitespace

--- WebApplicationCICD/Controllers/WeatherForecastController.cs
+++ WebApplicationCICD/Controllers/WeatherForecastController.cs
@@ -35,9 +35,7 @@ namespace WebApplicationCICD.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-
-
-                Date =          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
